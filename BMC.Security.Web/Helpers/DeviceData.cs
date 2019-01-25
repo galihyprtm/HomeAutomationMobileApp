89d89f173d1609cd120e244cce1d2a8af9c7f496
Gravicode Multinovative Plexindo <mifmasterz@yahoo.com>
2019-01-25 19:23:51 +0700

    add new

--- BMC.Security.Web/Helpers/DeviceData.cs
+++ BMC.Security.Web/Helpers/DeviceData.cs
@@ -21,11 +21,11 @@ namespace BMC.Security.Web.Helpers
                 new DeviceData (){ ID=4, Name="Kipas Musolla", IP="192.168.1.31" },
                 new DeviceData (){ ID=5, Name="Kipas Printer 3D", IP="192.168.1.32" },
                 new DeviceData (){ ID=6, Name="Kipas Depan Dapur", IP="192.168.1.33" },
-                new DeviceData (){ ID=7, Name="Lampu Musolla", IP="192.168.1.35" },
-                new DeviceData (){ ID=8, Name="Lampu Ruang Tamu", IP="192.168.1.26" },
-                new DeviceData (){ ID=6, Name="Kipas Kamar Depan", IP="192.168.1.36" },
+                //new DeviceData (){ ID=7, Name="Lampu Musolla", IP="192.168.1.35" },
+                new DeviceData (){ ID=7, Name="Lampu Ruang Tamu", IP="192.168.1.26" },
+                new DeviceData (){ ID=8, Name="Kipas Kamar Depan", IP="192.168.1.36" },
 
-                   new DeviceData (){ ID=7, Name="Lampu Musola", IP="192.168.1.29" }
+                   new DeviceData (){ ID=9, Name="Lampu Musola", IP="192.168.1.29" }
             };
         }
     }
