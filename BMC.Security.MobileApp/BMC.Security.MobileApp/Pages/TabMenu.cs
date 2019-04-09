27b2ad7091ec6ec3b5f0b9788704731a555ac275
galihyprtm <44695956+galihyprtm@users.noreply.github.com>
2019-04-09 16:11:12 +0700

    create mobile app

---
+++ BMC.Security.MobileApp/BMC.Security.MobileApp/Pages/TabMenu.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+namespace BMC.Security.MobileApp.Pages
+{
+    public class TabMenu : TabbedPage
+    {
+        public TabMenu()
+        {
+            this.BarBackgroundColor = Color.FromHex("#3499EC");
+            this.BarTextColor = Color.White;            
+            Children.Add(new Home());
+            Children.Add(new Contact());
+        }
+    }
+}
